e4fd6f0a711dc0ead85a2f9b6728cd6258c35abd
Sogolon Vieira <sogolonmd@gmail.com>
2020-07-03 02:14:23 -0300

    Storing accounts into database.

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
 
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/log-in', (req, res) => {
     return res.json('Log in');
 });
 
-router.get('/sign-up', (req, res) => {
-    return res.json('Sign up');
+router.get('/sign-up', async (req, res) => {
+
+    const email = 'sogolonmd@gmail.com'
+    const password = 'sha256'
+
+    
+    const hash = bcrypt.hashSync(password, saltRounds);
+
+
+    const result = await Account.create({ email, password: hash });
+    console.log(result);
+
+    return res.json(result);
 });
 
 module.exports = router;
