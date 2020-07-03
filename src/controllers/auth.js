99cb4bc8609121468f4d371ea2ef62672eb37a44
Sogolon Vieira <sogolonmd@gmail.com>
2020-07-03 03:29:50 -0300

    Account sign-up storing data in database

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -12,17 +12,18 @@ router.get('/log-in', (req, res) => {
 
 router.get('/sign-up', async (req, res) => {
 
-    const email = 'sogolonmd@gmail.com'
-    const password = 'sha256'
+    const { email, password } = req.body;
 
+    console.log({email, password});
     
+    const account = await Account.findOne({ where: { email }});
+    if (account)  return res.json('Account already exists');
+
     const hash = bcrypt.hashSync(password, saltRounds);
+    const newAccount = await Account.create({ email, password: hash });
+    console.log(newAccount);
 
-
-    const result = await Account.create({ email, password: hash });
-    console.log(result);
-
-    return res.json(result);
+    return res.json(newAccount);
 });
 
 module.exports = router;
