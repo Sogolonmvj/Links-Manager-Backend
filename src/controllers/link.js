6d0c21182fc43db5e59acef56c06ef3d46cfadf2
Sogolon Vieira <sogolonmd@gmail.com>
2020-07-08 02:08:47 -0300

    Links CRUD

--- src/controllers/link.js
+++ src/controllers/link.js
@@ -1,9 +1,66 @@
 const express = require('express');
 
+const { Link } = require('../models');
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    return res.jsonOK('Links');
+    const accountId = 1;
+    const links = await Link.findAll({ where: { accountId } });
+
+
+
+    return res.jsonOK(links);
 });
+
+router.get('/:id', async (req, res) => {
+    const accountId = 1;
+    const { id } = req.params;
+    const link = await Link.findOne({ where: { id, accountId } });
+    if (!link) return res.jsonNotFound();
+    return res.jsonOK(link);
+})
+
+router.post('/', async (req, res) => {
+    const accountId = 1;
+
+    const { label, url, isSocial } = req.body;
+
+    const image = 'https://google.com/image.jpg';
+
+    const link = await Link.create({ label, url, isSocial });
+
+    return res.jsonOK(link);
+});
+
+router.put('/:id', async (req, res) => {
+    const accountId = 1;
+    const { id } = req.params();
+    const { body } = req;
+    const fields = ['label', 'url', 'isSocial'];
+
+    const link = await Link.findOne({ where: { id: id, accountId: accountId }});
+
+    if (!link) return res.jsonNotFound();
+
+    fields.map(fieldName => {
+        const newValue = body[fieldName];
+        if(newValue != undefined) link[fieldName] = newValue;
+    });
+
+    await link.save();
+
+    return res.jsonOK(link);
+
+
+})
+
+router.delete('/:id', async (req, res) => {
+    const accountId = 2;
+    const link = await Link.findOne({ where: { id, accountId } });
+    if (!link) return res.jsonNotFound();
+    await link.destroy();
+    return res.jsonOK();
+})
 
 module.exports = router;
