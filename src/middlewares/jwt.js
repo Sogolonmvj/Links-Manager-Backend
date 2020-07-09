2ee14d436c5bb0ae0e0491c133c2cd7191e48be7
Sogolon Vieira <sogolonmd@gmail.com>
2020-07-08 21:08:03 -0300

    Excluded auth paths from JWT

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,15 @@
 const { verifyJwt } = require('../helpers/jwt');
 
 const checkJwt = (req, res, next) => {
+    // /auth/log-in
+    // /auth/sign-up
+
+    const { url: path } = req;
+    console.log(path);
+
+    const excludedPaths = ['/auth/log-in', '/auth/sign-up'];
+    const isExcluded = !!excludedPaths.find((p) => p.startsWith(path));
+    if (isExcluded) return next();
 
     let token = req.headers['authorization'];
 
